af6abf76e46f263997dac6c3174ebf7352c4e806
Peter Montano <pete225848@gmail.com>
2021-04-30 08:14:27 -0700

    unfinished update

--- app.js
+++ app.js
@@ -3,3 +3,31 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+let dexteririty = document.getElementById('input');
+
+function increment() {
+    document.getElementById('input').stepUp();
+}
+ 
+function decrement() {
+    document.getElementById('input').stepDown();
+}
+
+// Run this on an event handler on the buttons
+str-input.addEventListener('click', () => {
+function strength() {
+    let value = document.getElementById('str-input').value;
+    let img = document.getElementById('strong');
+});
+   /* if ('str-input' >= 9 ) {
+        img.src = 'strong';
+    } else if ('str-input' <= 7){
+        img.src = 'kinda-strong';
+    } else { ('str-input' <= 4){
+        img.src = 'weak';
+    }
+
+    }
+
+}
